d745413cd8dfd313cc85b7c0af5bdda8738e5ea5
Allender <alexander.a.borodin@hotmail.com>
2016-12-22 21:29:57 +0100

    Introduced ViewModel

--- CanvasApp/App.xaml.cs
+++ CanvasApp/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 
 namespace CanvasApp
 {
@@ -13,5 +15,26 @@ namespace CanvasApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var aMainWindow = new MainWindow();
+
+            var aMainViewModel = new MainWindowViewModel();
+
+            aMainViewModel.SketcherBackground = new BitmapImage(new Uri("/Resources/Images/test.jpg", UriKind.Relative));
+            aMainViewModel.SketcherBackground.CacheOption = BitmapCacheOption.None;
+
+            aMainWindow.DataContext = aMainViewModel;
+            aMainWindow.Show();
+        }
+
+        protected override void OnLoadCompleted(NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+
+            
+        }
     }
 }
---
+++ CanvasApp/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CanvasApp
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private BitmapImage m_SketcherBackground;
+
+        public BitmapImage SketcherBackground
+        {
+            get { return m_SketcherBackground; }
+            set
+            {
+                m_SketcherBackground = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
